71ae8bf7ae5bff22cdeef3cff34f7b9f4cebfa61
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2020-04-05 12:23:42 -0700

    lidar tester

--- tests/ol_lidar.cpp
+++ tests/ol_lidar.cpp
@@ -6,17 +6,20 @@
 #include "drivers/lidar/RPLidar.h"
 
 int main(int argc, char **argv){
-    
-    std::cout << "Connecting..." << std::endl;
+    char* port = NULL;
 
     LidarDriver* lidar = new RPLidar();
 
-    char* port = lidar->getPort(true);
-    std::cout << "RPLidar found at: " << port << std::endl;
+    if (argc > 1)
+        port = argv[1];
+
+    if (!port)
+        port = lidar->getPort(true);
+
+    std::cout << "Connecting RPLidar found at: " << port << std::endl;
 
     lidar->connect(port, true);
     usleep(10000);
-
     lidar->disconnect();
 
     return 0;
