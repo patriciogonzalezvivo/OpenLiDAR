79230782025cb1d83fdb0aca93d48207ed02484f
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2020-04-05 13:43:10 -0700

    Celestron test is more verbosed

--- tests/ol_celestron.cpp
+++ tests/ol_celestron.cpp
@@ -6,22 +6,28 @@
 #include "drivers/mount/Celestron.h"
 
 int main(int argc, char **argv){
-    
-    std::cout << "Connecting..." << std::endl;
+    char* port = NULL;
 
     MountDriver* mount = new Celestron();
 
-    char* port = mount->getPort(true);
-    std::cout << "Celestron mount found at: " << port << std::endl;
+    if (argc > 1)
+        port = argv[1];
 
-    mount->connect(port, true);
-    usleep(10000);
+    if (!port)
+        port = mount->getPort(true);
 
-    std::cout << "Az: " << mount->getAz() << std::endl;
-    std::cout << "Alt: " << mount->getAlt() << std::endl;
-    std::cout << "Offset: " << mount->getOffset().x << "," << mount->getOffset().y << "," << mount->getOffset().z << std::endl;
+    std::cout << "Connecting Celestron mount at: " << port << std::endl;
 
-    mount->disconnect();
+    if (mount->connect(port, true)) {
+        std::cout << "SUCESS connecting to " << port << std::endl;
+        usleep(10000);
+        std::cout << "Az: " << mount->getAz() << std::endl;
+        std::cout << "Alt: " << mount->getAlt() << std::endl;
+        std::cout << "Offset: " << mount->getOffset().x << "," << mount->getOffset().y << "," << mount->getOffset().z << std::endl;
+        mount->disconnect();
+    }
+    else 
+        std::cout << "FAIL connecting to " << port << std::endl;
 
     return 0;
 }
